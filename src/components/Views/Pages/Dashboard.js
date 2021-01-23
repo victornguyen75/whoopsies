1079d5d8d63639fd3f7fd566ad48d99f1a4c0288
Nguyen, Victor <vdn140030@gmail.com>
2021-01-22 18:11:48 -0600

    style(App.css, Dashboard.js): implement Material UI design and components; remove App.css styling for now

--- src/components/Views/Pages/Dashboard.js
+++ src/components/Views/Pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
 import TaskCreation from "../../UI/TaskCreation";
 import List from "../../UI/List";
 
@@ -33,11 +34,29 @@ export default function Dashboard() {
   return (
     <>
       <TaskCreation />
-      <List header={TO_DO_HEADER} tasks={tasks} />
-      <List header={IN_ANALYSIS_HEADER} />
-      <List header={IN_DEVELOPMENT_HEADER} />
-      <List header={IN_TESTING_HEADER} />
-      <List header={DONE_HEADER} />
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="flex-start"
+        spacing={7}
+      >
+        <Grid key="0" item>
+          <List header={TO_DO_HEADER} tasks={tasks} />
+        </Grid>
+        <Grid key="1" item>
+          <List header={IN_ANALYSIS_HEADER} />
+        </Grid>
+        <Grid key="2" item>
+          <List header={IN_DEVELOPMENT_HEADER} />
+        </Grid>
+        <Grid key="3" item>
+          <List header={IN_TESTING_HEADER} />
+        </Grid>
+        <Grid key="4" item>
+          <List header={DONE_HEADER} />
+        </Grid>
+      </Grid>
     </>
   );
 }
