0fc227d37d4b2b6df7f79bdce249c66096707ea0
Nguyen, Victor <vdn140030@gmail.com>
2021-01-22 19:51:37 -0600

    style(TaskCreation.js): add material-ui design

--- src/components/UI/TaskCreation.js
+++ src/components/UI/TaskCreation.js
@@ -1,12 +1,33 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function TaskCreation() {
   return (
     <form>
-      <input />
-      <input />
-      <input />
-      <button type="button">Create</button>
+      <div>
+        <TextField
+          label="Name"
+          variant="filled"
+          size="small"
+          style={{ marginRight: "1em" }}
+        />
+        <TextField
+          label="Priority"
+          variant="filled"
+          size="small"
+          style={{ marginRight: "1em" }}
+        />
+        <TextField
+          label="Release"
+          variant="filled"
+          size="small"
+          style={{ marginRight: "1em" }}
+        />
+        <Button variant="contained" color="primary" type="button" size="large">
+          Create
+        </Button>
+      </div>
     </form>
   );
 }
