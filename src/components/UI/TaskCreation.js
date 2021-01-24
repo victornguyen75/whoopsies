d27aaf4571da941f3e2d6fbfea924ea2f7f926d5
Nguyen, Victor <vdn140030@gmail.com>
2021-01-23 21:24:43 -0600

    feat(TaskCreation.js, Dashboard.js): add new tasks to the list

--- src/components/UI/TaskCreation.js
+++ src/components/UI/TaskCreation.js
@@ -1,35 +1,74 @@
 import React from "react";
+import PropTypes from "prop-types";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
-export default function TaskCreation() {
+export default function TaskCreation({
+  name,
+  setName,
+  priority,
+  setPriority,
+  release,
+  setRelease,
+  addTask,
+}) {
   const textStyles = { marginRight: "1em" };
 
   return (
-    <form>
-      <div>
-        <TextField
-          label="Name"
-          variant="filled"
-          size="small"
-          style={textStyles}
-        />
-        <TextField
-          label="Priority"
-          variant="filled"
-          size="small"
-          style={textStyles}
-        />
-        <TextField
-          label="Release"
-          variant="filled"
-          size="small"
-          style={textStyles}
-        />
-        <Button variant="contained" color="primary" type="button" size="large">
-          Create
-        </Button>
-      </div>
+    <form onSubmit={addTask}>
+      <TextField
+        label="Name"
+        variant="filled"
+        size="small"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={textStyles}
+      />
+      <TextField
+        label="Priority"
+        variant="filled"
+        size="small"
+        value={priority}
+        onChange={(e) => setPriority(e.target.value)}
+        style={textStyles}
+      />
+      <TextField
+        label="Release"
+        variant="filled"
+        size="small"
+        value={release}
+        onChange={(e) => setRelease(e.target.value)}
+        style={textStyles}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        size="large"
+        disabled={!name}
+      >
+        Create
+      </Button>
     </form>
   );
 }
+
+TaskCreation.propTypes = {
+  name: PropTypes.string,
+  setName: PropTypes.func,
+  priority: PropTypes.string,
+  setPriority: PropTypes.func,
+  release: PropTypes.string,
+  setRelease: PropTypes.func,
+  addTask: PropTypes.func,
+};
+
+TaskCreation.defaultProps = {
+  name: "TBD",
+  setName: () => {},
+  priority: "",
+  setPriority: () => {},
+  release: "TBD",
+  setRelease: () => {},
+  addTask: () => {},
+};
--- src/components/Views/Pages/Dashboard.js
+++ src/components/Views/Pages/Dashboard.js
@@ -1,28 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import TaskCreation from "../../UI/TaskCreation";
 import TaskList from "../../UI/TaskList";
-
-const tasks = [
-  {
-    id: 1 + Math.random(),
-    name: "dummy task 1",
-    priority: Math.floor(100 * Math.random()),
-    release: "12/12/2021",
-  },
-  {
-    id: 1 + Math.random(),
-    name: "dummy task 1",
-    priority: Math.floor(100 * Math.random()),
-    release: "12/12/2021",
-  },
-  {
-    id: 1 + Math.random(),
-    name: "dummy task 1",
-    priority: Math.floor(100 * Math.random()),
-    release: "12/12/2021",
-  },
-];
 
 export default function Dashboard() {
   const TO_DO_HEADER = "TO DO";
@@ -32,9 +11,47 @@ export default function Dashboard() {
   const DONE_HEADER = "DONE";
   const gridStyles = { margin: "0px" };
 
+  const [name, setName] = useState("");
+  const [priority, setPriority] = useState("");
+  const [release, setRelease] = useState("");
+  const [tasks, setTasks] = useState([
+    {
+      id: 1 + Math.random(),
+      name: "dummy task 1",
+      priority: Math.floor(100 * Math.random()).toString(),
+      release: "12/12/2021",
+    },
+  ]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+
+    const newList = [...tasks];
+
+    newList.push({
+      id: 1 + Math.random(),
+      name,
+      priority,
+      release,
+    });
+
+    setTasks(newList);
+    setName("");
+    setPriority("");
+    setRelease("");
+  };
+
   return (
     <>
-      <TaskCreation />
+      <TaskCreation
+        name={name}
+        setName={setName}
+        priority={priority}
+        setPriority={setPriority}
+        release={release}
+        setRelease={setRelease}
+        addTask={addTask}
+      />
       <Grid
         container
         direction="row"
