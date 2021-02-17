bbfafdf4c0f56d31d3ddf8169ffd6823a4ed9a2d
Nguyen, Victor <vdn140030@gmail.com>
2021-02-17 17:52:20 -0600

    feat(InDevelopmentPageViewModel.js, InDevelopmentPageView.js): add a fallback page if anyone tries to access General Questions or any other route

---
+++ src/components/ViewModels/InDevelopmentPageViewModel.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import useNotifications from "./CustomHooks/useNotifications";
+
+export default function InDevelopmentPageViewModel() {
+  const {
+    showNotification,
+    notificationText,
+    toggleNotification,
+  } = useNotifications();
+
+  useEffect(() => {
+    toggleNotification("Info: sorry this page does not exist... yet!");
+  }, [toggleNotification]);
+
+  return { showNotification, notificationText };
+}
---
+++ src/components/Views/InDevelopmentPageView.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Notification from "../UI/Notification";
+
+export default function InDevelopmentPage({ viewModel }) {
+  return (
+    <>
+      <h1>Whoopsies! This page does not exist yet. Sorry about that! 😅</h1>
+      <Notification
+        show={viewModel.showNotification}
+        text={viewModel.notificationText}
+      />
+    </>
+  );
+}
+
+InDevelopmentPage.propTypes = {
+  viewModel: PropTypes.shape({
+    showNotification: PropTypes.bool,
+    notificationText: PropTypes.string,
+  }),
+};
+
+InDevelopmentPage.defaultProps = {
+  viewModel: {
+    showNotification: false,
+    notificationText: "",
+  },
+};
