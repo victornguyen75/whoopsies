a7e65f25638078d060b347b6f2dc06dd842ee9a4
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-03-04 17:52:58 -0600

    test(DashboardPageView.test.js): update unit tests

--- src/components/Views/DashboardPageView.test.js
+++ src/components/Views/DashboardPageView.test.js
@@ -7,20 +7,18 @@ import DashboardPageView from "./DashboardPageView";
 afterEach(cleanup);
 
 describe("DashboardPageView", () => {
-  const viewModelTest1 = {
-    tasks: [
-      {
-        id: 1,
-        name: "Test 1",
-        status: "TO DO",
-      },
-      {
-        id: 2,
-        name: "Test 2",
-        status: "IN ANALYSIS",
-      },
-    ],
-  };
+  const tasks = [
+    {
+      id: 1,
+      name: "Test 1",
+      status: "TO DO",
+    },
+    {
+      id: 2,
+      name: "Test 2",
+      status: "IN ANALYSIS",
+    },
+  ];
 
   it("renders without crashing", () => {
     const div = document.createElement("div");
@@ -28,9 +26,7 @@ describe("DashboardPageView", () => {
   });
 
   it("filters the tasks in the proper lists", () => {
-    const { getByTestId } = render(
-      <DashboardPageView viewModel={viewModelTest1} />
-    );
+    const { getByTestId } = render(<DashboardPageView tasks={tasks} />);
     const textOfDOM = getByTestId("dashboard-page-view").textContent;
     expect(textOfDOM).to.have.string("TO DO (1)");
     expect(textOfDOM).to.have.string("IN ANALYSIS (1)");
