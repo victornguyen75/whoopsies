efbf0ca5dad28bd833fe497f2dcfdbcde3a188ea
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-03-15 16:24:26 -0500

    feat(TaskFormFields.js, TaskFormStyles.js): add project field and adjust styles for the form

--- src/components/UI/TaskFormStyles.js
+++ src/components/UI/TaskFormStyles.js
@@ -3,6 +3,7 @@ import FormControlLabelUnstyled from "@material-ui/core/FormControlLabel";
 import styled from "styled-components";
 
 const TextField = styled(TextFieldUnstyled)`
+  width: ${({ label }) => (label === "Sprint" ? "8em" : "initial")};
   margin: 0 10px 10px 0 !important;
 `;
 
